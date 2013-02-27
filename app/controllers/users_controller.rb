702b4d68b99ac0db7b0c985cefa5c35d5c5b7292
bestofwon <ddalpang22@naver.com>
2013-02-27 16:48:34 +0900

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+
+  before_filter :signed_in_user, only: [:index, :edit, :update]
+  before_filter :correct_user, only: [:edit, :update]
+  before_filter :admin_user, only: :destroy
   
   def show
     @user = User.find(params[:id])
@@ -11,12 +15,57 @@ class UsersController < ApplicationController
   def create
   		@user = User.new(params[:user])
   		if @user.save
-  			flash[:succes] = "Welcome to the Sample App"
-  			redirect_to @user
-  			#Handle a successful save.
+        sign_in @user
+  			 flash[:succes] = "Welcome to the Sample App"
+  			 redirect_to @user
   		else
-  			render 'new'
+  			 render 'new'
   		end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+   def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
+  end
+
+
+  private
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in." unless signed_in?
+        #flash[:notice] = "~"
+        #redirect_to signin)url과 같음
+      end
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
 end
